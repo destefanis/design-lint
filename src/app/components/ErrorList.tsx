b7e7b66d4498930a74fd29606683a42ce0fa9a4e
Daniel Destefanis <destefanis.drdestefanis@gmail.com>
2019-12-01 16:07:59 -0800

    Cleaned up icons, added error types, adjusted misc styling

--- src/app/components/ErrorList.tsx
+++ src/app/components/ErrorList.tsx
@@ -2,8 +2,20 @@ import * as React from "react";
 
 function ErrorList(props) {
   console.log(props);
+
+  // function setErrorIcon(type) {
+  //   if (type === "fill") {
+  //     return ()
+  //   }
+  // };
+
   const errorListItems = props.errors.map(error => (
-    <li className="error-list-item">{error.message}</li>
+    <li className="error-list-item">
+      <span className="error-type">
+        <img src={require("../assets/" + error.type.toLowerCase() + ".svg")} />
+      </span>
+      {error.message}
+    </li>
   ));
 
   return <ul className="errors-list">{errorListItems}</ul>;
